38fa9912409c6560d72e747e16f68c416b90fdf0
Aditya <strynic.codes11@gmail.com>
2026-05-23 17:56:00 +0530

    Multiplication Table

---
+++ Basic Programming Logic/MultiplicationTable.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class MultiplicationTable{
+
+    public static void main(String[] args){
+
+    Scanner sc  = new Scanner(System.in);
+
+    System.out.print("Enter the number: ");
+
+    int num = sc.nextInt(); 
+
+    //print multiplication table 
+    System.out.println("Multiplication Table of " + num);
+    for (int i = 1; i <= 10; i++){
+        System.out.println(num + " x " + i + " = " + (num * i));
+    }
+    sc.close();
+
+    }
+
+
+}
